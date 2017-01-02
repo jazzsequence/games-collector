219beb56e9a3bb2b61aab1e52a74f96ca5e2af7d
Chris Reynolds <chris@jazzsequence.com>
2017-01-02 16:34:45 -0700

    add hooks for attribute taxonomy namespace

--- inc/namespace.php
+++ inc/namespace.php
@@ -14,6 +14,10 @@ namespace GC\GamesCollector;
  */
 function bootstrap() {
 	// Add all your plugin hooks here.
-	add_action( 'cmb2_init', __NAMESPACE__ . '\\Game\\fields' );
-	add_action( 'init',      __NAMESPACE__ . '\\Game\\register_cpt' );
+	add_action( 'cmb2_init',             __NAMESPACE__ . '\\Game\\fields' );
+	add_action( 'init',                  __NAMESPACE__ . '\\Game\\register_cpt' );
+	add_action( 'init',                  __NAMESPACE__ . '\\Attributes\\register_taxonomy' );
+	add_action( 'admin_init',            __NAMESPACE__ . '\\Attributes\\create_default_attributes' );
+	add_action( 'add_meta_boxes',        __NAMESPACE__ . '\\Attributes\\metabox' );
+	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\Attributes\\enqueue_scripts' );
 }
