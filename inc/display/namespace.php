a687ba300b76ac95394e45d73d80ea4aa5f4b64b
Chris Reynolds <chris@jazzsequence.com>
2017-01-03 16:58:39 -0700

    first pass at shortcode output

--- inc/display/namespace.php
+++ inc/display/namespace.php
@@ -9,3 +9,47 @@
  */
 
 namespace GC\GamesCollector\Display;
+use GC\GamesCollector\Game;
+
+function shortcode() {
+	$games = get_posts([
+		'posts_per_page' => -1,
+		'post_type'      => 'gc_game',
+		'orderby'        => 'title',
+	]);
+
+	ob_start(); ?>
+
+	<div class="games-collector-list">
+		<?php foreach ( $games as $game ) { ?>
+			<div class="game-single <?php echo esc_attr( $game->post_slug ); ?>" id="game-<?php echo absint( $game->ID ); ?>">
+				<?php // Output a link if one was saved.
+				if ( $link = get_post_meta( $game->ID, '_gc_link', true ) ) { ?>
+					<a href="<?php echo esc_url( $link ); ?>">
+				<?php } ?>
+
+				<span class="game-title" id="game-<?php echo absint( $game->ID ); ?>-title"><?php echo wp_kses_post( $game->post_title ); ?></span>
+
+				<?php if ( $link ) { ?>
+					</a>
+				<?php }
+
+				$players_min_max = Game\get_players_min_max( $game->ID );
+				// Output game info. ?>
+
+				<div class="game-info" id="game-<?php echo absint( $game->ID ); ?>-info">
+					<?php if ( isset( $players_min_max['min'] ) ) { ?>
+						<span class="game-min-players" id="game-<?php echo absint( $game->ID ); ?>-min-players"><?php echo absint( $players_min_max['min'] ); ?></span>
+					<?php } ?>
+					<?php if ( isset( $players_min_max['min'] ) && isset( $players_min_max['max'] ) ) { ?>
+						-
+						<span class="game-max-players" id="game-<?php echo absint( $game->ID ); ?>-max-players"><?php echo absint( $players_min_max['max'] ); ?></span>
+					<?php } ?>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
+
+	<?php $content = ob_get_clean();
+	return $content;
+}
