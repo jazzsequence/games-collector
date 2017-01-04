6007c8902de260f1fa0c46229eb84f422d648a05
Chris Reynolds <chris@jazzsequence.com>
2017-01-04 15:51:03 -0700

    add javascript to handle filtering by dropdowns

--- assets/js/games-collector.js
+++ assets/js/games-collector.js
@@ -25,12 +25,16 @@ window.GamesCollector = {};
 					horizontalAlignment: 0
 				}
 			}),
+			playersFilter: $('.players-filter-select'),
+			difficultyFilter: $('.difficulty-filter-select'),
 		};
 	};
 
 	// Combine all events.
 	plugin.bindEvents = function() {
 		plugin.$c.buttons.on( 'click', 'button', plugin.filter );
+		plugin.$c.playersFilter.on( 'change', plugin.filterDropdown );
+		plugin.$c.difficultyFilter.on( 'change', plugin.filterDropdown );
 	};
 
 	// Do we meet the requirements?
@@ -38,9 +42,14 @@ window.GamesCollector = {};
 		return plugin.$c.list.length;
 	};
 
-
 	plugin.filter = function() {
 		let filterValue = $(this).attr( 'data-filter' );
+		plugin.$c.grid.isotope({ filter: filterValue });
+	}
+
+	plugin.filterDropdown = function() {
+		// get filter value from option value
+		var filterValue = this.value;
 		plugin.$c.grid.isotope({ filter: filterValue });
 	}
 
