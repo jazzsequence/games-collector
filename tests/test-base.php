be90990621981613c43b5217ab37f5a4f942621a
Chris Reynolds <chris@jazzsequence.com>
2017-01-12 14:19:24 -0700

    add base unit tests for plugin

---
+++ tests/test-base.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Unit tests for base Game Collector plugin.
+ *
+ * @package GC\GamesCollector
+ */
+
+/**
+ * Games Collector base unit test class.
+ */
+class GC_Test_Game_Collector_Base extends WP_UnitTestCase {
+	/**
+	 * Ensure the plugin is loaded and the callback is registered to 'plugins_loaded'.
+	 */
+	function test_plugin_loaded() {
+		$this->assertTrue( function_exists( 'gc_the_games' ) );
+		$this->assertEquals( has_action( 'plugins_loaded', 'GC\GamesCollector\bootstrap' ) );
+	}
+}
