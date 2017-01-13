8db2ae48c37878619d166a7b584c33648a24c5a8
Chris Reynolds <chris@jazzsequence.com>
2017-01-13 16:20:33 -0700

    test that the attribute taxonomy exists

--- tests/test-attributes.php
+++ tests/test-attributes.php
@@ -11,5 +11,16 @@ use GC\GamesCollector\Attributes;
  * Games Collector Attributes unit test class.
  */
 class GC_Test_Attributes extends WP_UnitTestCase {
+	/**
+	 * Test that the Attribute taxonomy was created.
+	 *
+	 * @covers GC\GamesCollector\Attributes\register_taxonomy
+	 */
+	function test_attributes_taxonomy_exists() {
+		$this->assertTrue(
+			taxonomy_exists( 'gc_attribute' ),
+			'The Game Attributes taxonomy does not exist.'
+		);
+	}
 
 }
