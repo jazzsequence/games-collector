13f1a5147479726c0cbcdbdc7d87df2cb9fe614c
Chris Reynolds <chris@jazzsequence.com>
2017-01-13 16:12:36 -0700

    adds unit tests for attributes namespace

---
+++ tests/test-attributes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Unit tests for attributes namespace.
+ *
+ * @package GC\GamesCollector
+ */
+
+use GC\GamesCollector\Attributes;
+
+/**
+ * Games Collector Attributes unit test class.
+ */
+class GC_Test_Attributes extends WP_UnitTestCase {
+
+}
