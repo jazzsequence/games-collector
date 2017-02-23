91f63014a21fbbec771982e53327eb33048dde1f
Chris Reynolds <chris@jazzsequence.com>
2017-02-23 16:33:03 -0700

    add initialn unit test framework for shortcode unit tests

---
+++ tests/test-shortcode.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Unit tests for Shortcode namespace.
+ *
+ * @since   1.1.0
+ * @package GC\GamesCollector
+ */
+
+use GC\GamesCollector\Shortcode;
+
+/**
+ * Games Collector Shortcode unit test class.
+ *
+ * @since 1.1.0
+ */
+class GC_Test_Shortcode extends WP_UnitTestCase {
+}
