63de3062fb8b3e891c284a220733961428cbf031
Chris Reynolds <chris@jazzsequence.com>
2017-02-28 14:30:18 -0700

    tests the attributes endpoints

--- tests/test-api.php
+++ tests/test-api.php
@@ -104,4 +104,32 @@ class GC_Test_Game_Collector_API extends WP_UnitTestCase {
 			'The post title for the API post did not match what we were expecting.'
 		);
 	}
+
+	/**
+	 * Test that the attributes endpoints can be reached.
+	 *
+	 * @since  1.1.0
+	 * @covers nothing
+	 */
+	public function test_attribute_api_endpoint() {
+		// Test that we can hit the endpoint.
+		$response      = wp_remote_get( get_home_url( null, '/wp-json/wp/v2/attributes/' ) );
+		$response_code = wp_remote_retrieve_response_code( $response );
+		$this->assertEquals(
+			$response_code,
+			200,
+			sprintf( 'Did not get a 200 OK status code fetching from the API. Response returned was %s: %s', $response_code, wp_remote_retrieve_response_message( $response ) )
+		);
+
+		// Test that the API can return data on a specific attribute.
+		Attributes\create_default_attributes();
+		$term     = get_term_by( 'name', 'Fantasy', 'gc_attribute' );
+		$api_term = json_decode( wp_remote_retrieve_body( wp_remote_get( get_home_url( null, '/wp-json/wp/v2/attributes/' . $this->get_attribute_id_by_title( 'Fantasy' ) ) ) ) );
+		$this->assertSame(
+			$term->name,
+			$api_term->name,
+			'Tried to get the attribute "Fantasy" via the API but was not able to retrieve attribute information from the API.'
+		);
+
+	}
 }
