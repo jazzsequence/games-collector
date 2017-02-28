9a3b0f63bdc355011c5204124bfbe8bc27ee74e1
Chris Reynolds <chris@jazzsequence.com>
2017-02-28 14:37:39 -0700

    adds new namespace for API stuff

---
+++ inc/api/namespace.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Games Collector Api.
+ *
+ * Adds API endpoints for Games Collector games.
+ *
+ * @package GC\GamesCollector\Api
+ * @since   1.1.0
+ */
+
+namespace GC\GamesCollector\Api;
