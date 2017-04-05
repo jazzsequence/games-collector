d9e3e68d900a6e55eabf2da4353ad474495827b5
Chris Reynolds <chris@jazzsequence.com>
2017-04-05 10:37:00 -0600

    defined post_ids so we don't possibly end up with an undefined variable

--- inc/shortcode/namespace.php
+++ inc/shortcode/namespace.php
@@ -26,8 +26,9 @@ function shortcode( $atts ) {
 	], $atts );
 
 	// Get the ID from the atts, if one was set, so we can get a single game (or all games).
+	$post_ids = [];
 	if ( '' === $atts['gc_game'] ) {
-		$post_ids = [];
+		$post_ids = $post_ids;
 	} elseif ( is_array( $atts['gc_game'] ) ) {
 		$post_ids = $atts['gc_game'];
 	} elseif ( false !== strpos( (string) $atts['gc_game'], ',' ) ) {
