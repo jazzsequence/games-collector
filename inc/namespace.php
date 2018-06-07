4852be3dbce454a9a521a1333eb3ecf1360e1a3b
Chris Reynolds <chris@jazzsequence.com>
2018-06-07 12:12:59 -0600

    add a filter to alter the number of players if a game only allows a specific number

--- inc/namespace.php
+++ inc/namespace.php
@@ -24,6 +24,7 @@ function bootstrap() {
 	add_action( 'register_shortcode_ui',   __NAMESPACE__ . '\\Shortcode\\register_all_games_shortcode' );
 	add_action( 'register_shortcode_ui',   __NAMESPACE__ . '\\Shortcode\\register_individual_games_shortcode' );
 	add_filter( 'rest_prepare_gc_game',    __NAMESPACE__ . '\\Api\\filter_games_json', 10, 2 );
+	add_filter( 'gc_filter_players',       __NAMESPACE__ . '\\specific_number_of_players', 10, 3 );
 	add_shortcode( 'games-collector',      __NAMESPACE__ . '\\Shortcode\\shortcode' );
 	add_shortcode( 'games-collector-list', __NAMESPACE__ . '\\Shortcode\\shortcode' );
 }
@@ -42,4 +43,34 @@ function activate() {
 			'post_status'  => 'publish',
 		] );
 	}
+}
+
+/**
+ * Update the number of players if a game only allows a specific number.
+ *
+ * @since  1.2.0
+ * @param  int    $game_id         The game's post ID.
+ * @param  array  $players_min_max The minimum and maximum number of players.
+ * @param  string $output          The player output.
+ * @return string                  The filtered output.
+ */
+function specific_number_of_players( $game_id, $players_min_max, $output ) {
+	if ( absint( $players_min_max['min'] ) === absint( $players_min_max['max'] ) ) {
+		ob_start(); ?>
+
+		<span class="gc-icon icon-game-players"><?php Display\the_svg( 'players', false ); ?></span><span class="game-num-players" id="game-<?php echo absint( $game_id ); ?>-num-players">
+		<?php
+		echo esc_attr( sprintf(
+			// Translators: %d is the number of players.
+			_n( '%d player', '%d players', absint( $players_min_max['min'] ), 'games-collector' ),
+			absint( $players_min_max['min'] )
+		) );
+		?>
+		</span>
+		<?php
+
+		$output = ob_get_clean();
+	}
+
+	return $output;
 }
