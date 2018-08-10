8fbea3582f4b013582a5a3317817c2e981cb1bc0
Chris Reynolds <chris@jazzsequence.com>
2018-08-10 10:36:39 -0600

    add functions to return the bgg endpoint url

--- inc/bgg/namespace.php
+++ inc/bgg/namespace.php
@@ -9,3 +9,23 @@
  */
 
 namespace GC\GamesCollector\BGG;
+
+/**
+ * Return the BGG v1 API endpoint.
+ *
+ * @since  1.2.0
+ * @return string The BGG v1 endpoint.
+ */
+function bgg_api() {
+	return esc_url( 'https://www.boardgamegeek.com/xmlapi/' );
+}
+
+/**
+ * Return the BGG v2 (beta) API endpoint.
+ *
+ * @since  1.2.0
+ * @return string The BGG v2 endpoint.
+ */
+function bgg_api2() {
+	return esc_url( 'https://www.boardgamegeek.com/xmlapi2/' );
+}
