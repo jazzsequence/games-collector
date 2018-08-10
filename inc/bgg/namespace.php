0ff596b1776fc00c3018288df9f56d21bbb20aed
Chris Reynolds <chris@jazzsequence.com>
2018-08-10 10:25:25 -0600

    add bgg integration namespace

---
+++ inc/bgg/namespace.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Games Collector BoardGameGeek API integration.
+ *
+ * Integrates BoardGameGeek's XML API into Games Collector to allow game data to be imported.
+ *
+ * @package GC\GamesCollector\BGG
+ * @since   1.2.0
+ */
+
+namespace GC\GamesCollector\BGG;
