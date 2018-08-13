e69db57a1b4666f6ddd8aca9a0b5a1bd26a8dc13
Chris Reynolds <chris@jazzsequence.com>
2018-08-13 15:39:37 -0600

    add bgg test class

---
+++ tests/test-bgg.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Unit tests for Game namespace.
+ *
+ * @since   1.0.0
+ * @package GC\GamesCollector
+ */
+
+use GC\GamesCollector\BGG;
+
+/**
+ * Games Collector Game unit test class.
+ *
+ * @since 1.2.0
+ */
+class GC_Test_BGG extends WP_UnitTestCase {
+
+}
