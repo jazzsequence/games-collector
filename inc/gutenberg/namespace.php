25337b2ba135ca362fa3b09b2db57a5416c439ae
Chris Reynolds <chris@jazzsequence.com>
2018-09-07 12:27:24 -0600

    enqueue the admin js and css for gberg

--- inc/gutenberg/namespace.php
+++ inc/gutenberg/namespace.php
@@ -7,3 +7,11 @@
  */
 
 namespace GC\GamesCollector\Gutenberg;
+
+/**
+ * Enqueue the Gutenberg editor js and css.
+ */
+function enqueue_block_editor_assets() {
+	wp_enqueue_script( 'games-collector-gberg-editor', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/js/editor.js', [ 'wp-i18n', 'wp-blocks', 'wp-element', 'wp-component' ], '1.3.0' );
+	wp_enqueue_style( 'games-collector-gberg-editor', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/css/editor.css', [ 'wp-blocks' ], '1.3.0' );
+}
