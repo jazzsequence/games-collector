5bbaf2356c3380367e583d0bd7e3e23c51d95ff1
Chris Reynolds <chris@jazzsequence.com>
2018-09-10 16:00:50 -0600

    load scss

--- assets/js/blocks/index.js
+++ assets/js/blocks/index.js
@@ -7,6 +7,9 @@
 // Define the textdomain.
 wp.i18n.setLocaleData( { '': {} }, 'games-collector' );
 
+// Load the editor styles.
+import '../../sass/editor.scss';
+
 // Load the Gutenberg icon.
 import icon from './icon';
 
