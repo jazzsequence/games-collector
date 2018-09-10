a6fa4f3943f5285ec61ee5b01f72145dfe919310
Chris Reynolds <chris@jazzsequence.com>
2018-09-10 16:02:42 -0600

    register a test block

--- assets/js/blocks/index.js
+++ assets/js/blocks/index.js
@@ -19,3 +19,51 @@ const { registerBlockType } = wp.blocks;
 const { Spinner } = wp.components;
 const { withSelect } = wp.data;
 
+registerBlockType( 'games-collector/add-all-games', {
+	title: __( 'All Games', 'games-collector' ),
+	description: __( 'Add all games to any post or page.', 'games-collector' ),
+	category: 'widgets',
+	icon: {
+		background: '#0073AA',
+		src: icon
+	},
+	keywords: [
+		__( 'Games Collector', 'games-collector' ),
+		__( 'game list', 'games-collector' ),
+		__( 'all games', 'games-collector' ),
+	],
+        attributes: {
+            message: {
+                type: 'array',
+                source: 'children',
+                selector: '.message-body',
+            }
+        },
+        edit: props => {
+            const { attributes: { message }, className, setAttributes } = props;
+            const onChangeMessage = message => { setAttributes( { message } ) };
+            return (
+                <div className={ className }>
+                    <h2>{ __( 'Call to Action', 'games-collector' ) }</h2>
+                    <RichText
+                        tagName="div"
+                        multiline="p"
+                        placeholder={ __( 'Add your custom message', 'games-collector' ) }
+                  		onChange={ onChangeMessage }
+                  		value={ message }
+              		/>
+                </div>
+            );
+        },
+        save: props => {
+            const { attributes: { message } } = props;
+            return (
+                <div>
+                    <h2>{ __( 'Call to Action', 'games-collector' ) }</h2>
+                    <div class="message-body">
+                        { message }
+                    </div>
+                </div>
+            );
+        },
+} );
