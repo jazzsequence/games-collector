792b5db93011d69621303331ed06d54d621bfad0
Chris Reynolds <chris@jazzsequence.com>
2018-09-14 14:52:28 -0600

    refactor the block to be the all games block includes everything we need to display all the games, just need some css magic

--- assets/js/blocks/index.js
+++ assets/js/blocks/index.js
@@ -49,63 +49,76 @@ registerBlockType( 'games-collector/add-all-games', {
 		__( 'game list', 'games-collector' ),
 		__( 'all games', 'games-collector' ),
 	],
-	save: props => {
-		return;
-	}
-});
-
-registerBlockType( 'games-collector/add-single-game', {
-		title: __( 'Single Game', 'games-collector' ),
-		description: __( 'Add a single game to a post or page.', 'games-collector' ),
-		category: 'widgets',
-		icon: {
-			src: icon
-		},
-		keywords: [
-			__( 'Games Collector', 'games-collector' ),
-			__( 'single game', 'games-collector' ),
-			__( 'add game', 'games-collector' ),
-		],
-        attributes: {
-            gameTitle: {
-                type: 'string',
-                source: 'children',
-                selector: '.game-name-input input',
+    edit: withSelect( query => {
+            return {
+                posts: query( 'core' ).getEntityRecords( 'postType', 'gc_game', { per_page: -1, orderby: 'title', order: 'asc' } )
+            };
+        } )( ( { posts, className, isSelected } ) => {
+            if ( ! posts ) {
+                return (
+                    <p className={ className } >
+                        <Spinner />
+                        { __( 'Loading Posts', 'games-collector' ) }
+                    </p>
+                );
             }
-        },
-        edit: props => {
-            const { attributes: { gameTitle }, className, setAttributes } = props;
-            const getGame = function( gameTitle ) {
-            	// Set the gameTitle to be the updated title that was entered into the form.
-            	gameTitle = setAttributes( { gameTitle } );
-
-            	// Create a new title from the updated gameTitle.
-            	let title = props.attributes.gameTitle;
-
-            	// This logs the game title as it's updated.
-            	console.log(props.attributes.gameTitle);
-            	// This logs the URL that is going to be hit.
-            	console.log(wpapi.games().search(title).toString());
-            	// This logs the response against a search for that game.
-            	console.log( wpapi.games().search(title));
-
-            	// Return the gameTitle that was set.
-            	return gameTitle;
+            if ( 0 === posts.length ) {
+                return <p>{ __( 'No Posts', 'games-collector' ) }</p>;
             }
             return (
                 <div className={ className }>
-                    <TextControl
-                    	className="game-name-input"
-                    	label={ __( 'Game', 'games-collector' ) }
-                        placeholder={ __( 'The title of the game, e.g. Star Realms', 'games-collector' ) }
-                  		onChange={ gameTitle => getGame( gameTitle ) }
-                  		value={ gameTitle }
-              		/>
+                    { posts.map( post => {
+                    	console.log(post);
+						let divId       = `game-${ post.id }-info`,
+							title       = {
+								id: `game-${ post.id }-title`,
+							},
+							numPlayers  = {
+								id: `game-${ post.id }-num-players`,
+								total: (
+									'undefined' === typeof post.max_players ||
+									post.min_players[0] === post.max_players[0]
+								) ? post.min_players[0] : `${ post.min_players[0] } - ${ post.max_players[0] }`,
+							},
+							playingTime = {
+								id: `game-${ post.id }-playing-time`,
+								message: `${ post.time } minutes`,
+							},
+							age         = {
+								id: `game-${ post.id }-age`,
+								message: `${ post.age }+`,
+							},
+							difficulty  = {
+								id: `game-${ post.id }-difficulty`,
+							},
+							attributes  = {
+								id: `game-${ post.id }-attributes`,
+								message: `${ post.attributes.join( ', ' ) }`,
+							};
+
+                        return (
+                            <div className={ className }>
+                                <a href={ post.link }>
+                                    <span className="game-title" id={ title.id }>{ post.title.rendered }</span>
+                                </a>
+                                <div className="game-info" id={ divId }>
+                        			<span className="gc-icon icon-game-players">{ icons.players }</span><span className="game-num-players" id={ numPlayers.id }>{ numPlayers.total }</span>
+                        			<span className="gc-icon icon-game-time">{ icons.time }</span><span className="game-playing-time" id={ playingTime.id }>{ playingTime.message }</span>
+                        			<span className="gc-icon icon-game-age">{ icons.age }</span><span className="game-age" id={ age.id }>{ age.message }</span>
+                        			<span className="gc-icon icon-game-difficulty">{ icons.difficulty }</span><span className="game-difficulty" id={ difficulty.id }>{ ucfirst( post.difficulty[0] ) }</span>
+                        			<div className="game-attributes">
+                        				<span className="gc-icon icon-game-attributes">{ icons.tags }</span><span className="game-attributes" id={ attributes.id }>{ attributes.message }</span>
+                        			</div>
+                                </div>
+                            </div>
+                        );
+                    }) }
                 </div>
             );
-        },
-        save: props => {
-            const { gameTitle } = props.attributes;
-            return gameTitle;
-        },
-} );
+        } ) // end withAPIData
+    , // end edit
+    save() {
+        // Rendering in PHP
+        return null;
+    },
+});
