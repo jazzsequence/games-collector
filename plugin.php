f7a1025fc9b94741e8acb38b1a1a37c9abd1a2cd
Chris Reynolds <chris@jazzsequence.com>
2018-12-10 09:15:40 -0700

    remove extended taxos require

--- plugin.php
+++ plugin.php
@@ -46,7 +46,6 @@ function autoload_init() {
 	// Add in some specific includes and vendor libraries.
 	$files = [
 		dirname( __FILE__ ) . '/vendor/extended-cpts/extended-cpts.php',
-		dirname( __FILE__ ) . '/vendor/extended-taxos/extended-taxos.php',
 		dirname( __FILE__ ) . '/vendor/cmb2/init.php',
 		dirname( __FILE__ ) . '/inc/namespace.php',
 		dirname( __FILE__ ) . '/inc/functions.php',
