f940d197b5d89c761aa7e063c9111d5fd311e5e3
Chris Reynolds <chris@humanmade.com>
2019-12-30 12:23:44 -0700

    require isotope

--- inc/display/namespace.php
+++ inc/display/namespace.php
@@ -382,8 +382,8 @@ function enqueue_scripts() {
 		return;
 	}
 
-	wp_enqueue_style( 'games-collector', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/css/main.css', [], '1.3.0' );
-	wp_enqueue_script( 'games-collector', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/js/main.js', [ 'jquery' ], '1.3.0' );
+	wp_enqueue_style( 'games-collector', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/css/main.css', [], '1.3.3' );
+	wp_enqueue_script( 'games-collector', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/js/main.js', [ 'jquery', 'isotope' ], '1.3.3' );
 	wp_enqueue_script( 'isotope', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', [], '3.0.6' );
 }
 
