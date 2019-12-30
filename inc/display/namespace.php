83b5c6636aaa936eafa710aa0bf0744b3e130dca
Chris Reynolds <chris@jazzsequence.com>
2019-12-30 12:26:46 -0700

    Merge updates from webgui

--- inc/display/namespace.php
+++ inc/display/namespace.php
@@ -384,7 +384,7 @@ function enqueue_scripts() {
 
 	wp_enqueue_style( 'games-collector', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/css/main.css', [], '1.3.3' );
 	wp_enqueue_script( 'games-collector', dirname( plugin_dir_url( __FILE__ ), 2 ) . '/assets/js/main.js', [ 'jquery', 'isotope' ], '1.3.3' );
-	wp_enqueue_script( 'isotope', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', [], '3.0.6' );
+	wp_enqueue_script( 'isotope', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', [], '3.0.6' );	
 }
 
 /**
