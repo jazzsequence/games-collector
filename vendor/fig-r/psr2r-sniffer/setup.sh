5b30c5807f836a7470a91bd9c8ec89599a53573c
Chris Reynolds <chris@jazzsequence.com>
2026-03-17 15:48:33 -0600

    chore: remove vendor directory from version control
    vendor/ is now gitignored. Dependencies are installed at release time
    via composer install in the release workflow.

--- vendor/fig-r/psr2r-sniffer/setup.sh
+++
@@ -1,8 +0,0 @@
-# Make sure this file is executable
-# chmod +x setup.sh
-
-# Download composer phar if not available yet
-[ ! -f composer.phar ] && curl -sS https://getcomposer.org/installer | php
-
-# Download phpunit phar if not available yet
-[ ! -f phpunit.phar ] && php composer.phar test-setup
--- vendor/yoast/phpunit-polyfills/src/Polyfills/AssertArrayWithListKeys_Empty.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Yoast\PHPUnitPolyfills\Polyfills;
-
-/**
- * Empty trait for use with PHPUnit >= 11.0.0 in which the polyfill is not needed.
- */
-trait AssertArrayWithListKeys {}
